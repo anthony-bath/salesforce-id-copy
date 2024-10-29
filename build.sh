bbe4fee12a4e278a49144502a11e3d3a3063fe9a
anthony-bath <44715475+anthony-bath@users.noreply.github.com>
2024-10-29 12:51:27 -0400

    feat: add icons

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 rm -rf dist
 npx esbuild src/background.ts src/offscreen/offscreen.ts --bundle --outdir=dist --format=iife --platform=browser
-cp src/manifest.json dist/manifest.json
+cp manifest.json dist/manifest.json
 cp src/offscreen/offscreen.html dist/offscreen.html
+mkdir -p dist/icons
+cp icons/* dist/icons/
